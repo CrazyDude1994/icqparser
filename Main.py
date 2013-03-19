c8aed43c98849edf5bb81ad4ec7698e2dbbfef24
Kartavtsev Sergey <crazyserega1994@mail.ru>
2013-03-19 11:28:33 +0200

    Signed-off-by: Kartavtsev Sergey <crazyserega1994@mail.ru>

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from httplib import HTTPConnection, HTTPResponse, HTTPException
+from httplib import HTTPConnection, HTTPException
 from ICQParser import ICQParser
 import sqlite3
 
@@ -14,20 +14,50 @@ class Application:
         try:
             self.cursor.execute("CREATE TABLE ICQList (ICQNumber text)")
         except Exception as e:
-            pass
+            print e
+            
         self.StartWork()
+        #self.ProcessErrors()
 
     def StartWork(self):
-        for i in range(1, self.parsePages):
+        for i in range(1, self.parsePages + 1):
             try:
-                self.connection.request('GET', '/memberlist.php?do=getall&page='+ str(i) +'&ltr=&pp=100&order=asc&postslower=0&postsupper=0&sort=username&ausername=&homepage=&icq=&aim=&yahoo=&msn=&joindateafter=&joindatebefore=&lastpostafter=&lastpostbefore=')
-                self.parser.feed(self.connection.getresponse().read())
+                self.connection.request('GET', '/memberlist.php?do=getall&page='+ str(i)
+                                         +'&ltr=&pp=100&order=asc&postslower=0&postsupper=0&sort=username&ausername=&homepage=&icq=&aim=&yahoo=&msn=&joindateafter=&joindatebefore=&lastpostafter=&lastpostbefore=')
+                response = self.connection.getresponse()
+                self.parser.feed(response.read())
                 self.ICQList.extend(self.parser.data)
                 print "Proccessed", i, "page.", str((float(i) / self.parsePages) * 100) + "% has done."
             except HTTPException as e:
                 self.errorsPage.append(i)
-                print "Error on", i
-
+                print "Error on", i, e
+                break
+                
+        for icq in self.ICQList:
+            self.cursor.execute("INSERT INTO ICQList VALUES (?)", (icq,))
+        self.sql.commit()
+        self.connection.close()
+        print "Changes commited!"
+        raw_input()
+                
+    def ProcessErrors(self):
+        while len(self.errorsPage) > 0:
+            tempErrorsPage = []
+            for i in self.errorsPage:
+                try:
+                    self.connection.request('GET', '/memberlist.php?do=getall&page='+ str(i)
+                                             +'&ltr=&pp=100&order=asc&postslower=0&postsupper=0&sort=username&ausername=&homepage=&icq=&aim=&yahoo=&msn=&joindateafter=&joindatebefore=&lastpostafter=&lastpostbefore=')
+                    response = self.connection.getresponse()
+                    self.parser.feed(response.read())
+                    self.ICQList.extend(self.parser.data)
+                    print "Proccessed", i, "error page."
+                except HTTPException:
+                    tempErrorsPage.append(i)
+                    print "Error on", i
+                except:
+                    print ""
+            self.errorsPage = tempErrorsPage[:]
+            
         for icq in self.ICQList:
             self.cursor.execute("INSERT INTO ICQList VALUES (?)", (icq,))
         self.sql.commit()
